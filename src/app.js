d4be3b033a6cbd149115f2340146b4233e7513ae
Zidan <zidandevs@gmail.com>
2025-01-07 21:32:38 +0700

    create module for proccessing data notes

--- src/app.js
+++ src/app.js
@@ -1,81 +1,17 @@
 import noteComponent from "./script/components/note.js";
-import masonryGrid from "./script/components/masonry.js"
-
-const STORAGE_KEY = 'notes';
-const notesArr = [];
+import masonryGrid from "./script/components/masonry.js";
+import {notes, loadDataStorage, insertNewNote} from "./script/data/notesData.js";
 
 function render(){
   const notesListContainer = document.querySelector('.notes-list');
   notesListContainer.innerHTML = "";
 
-  notesArr.forEach(note => {
+  notes.forEach(note => {
     notesListContainer.appendChild(noteComponent(note));
   })
 
   masonryGrid.reloadItems()
   masonryGrid.layout()
-}
-
-
-function addNewNote(){
-  const titleInput = document.querySelector('#title-input').value;
-  const bodyInput = document.querySelector('#note-body-input').value;
-  const newNote = generateNoteObject(titleInput, bodyInput);
-  
-  notesArr.push(newNote);
-  saveData();
-}
-
-// only call on first load, the rest will use noteArr memory
-function loadDataStorage(){
-  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if(data !== null){
-    for (const note of data) {
-      notesArr.push(note);
-    }
-
-  }
-}
-
-function saveData(){
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(notesArr));
-  render();
-  console.log(generateRandomColor())
-}
-
-function generateNoteObject(title, body){
-  return {
-    id: generateId(),
-    title,
-    body,
-    timestamp: Date.now(),
-    archived: false,
-    color: generateRandomColor()
-  }
-}
-
-function generateRandomColor(){
-  // array colors yg nanti akan diakses berdasarkan index random
-  const noteColors = ["taro", "teal", "blueberry", "emerald", "maroon", "yellow", "brown", "orange"];
-  const usedColors = notesArr.map(note => note.color); // arr warna yg sudah dipakai
-  const availableColors = noteColors.filter(color => !usedColors.includes(color)); // warna yang belum dipakai didapatkan dari filter usedcolor dan notecolors
-
-  // jika semua warna sudah dipakai buat random murni
-  if(availableColors.length == 0){
-    return noteColors[Math.floor(Math.random() * noteColors.length)];
-  }
-
-  // jika masih ada buat agar unique
-  return availableColors[Math.floor(Math.random() * availableColors.length)];
-
-  // TODO
-  // BUAT FUNGSI AGAR WARNA SELALU UNIQUE SETIAP BLOCK 
-  // SETIAP BLOCK ADALAH 8 NOTES(BERDASARKAN JUMLAH WARNA YG TERSEDIA)
-  // JADI SETIAP KELIPATAN 8 NOTES WARNA SELALU UNIQUE
-}
-
-function generateId(){
-  return Date.now().toString(36).slice(2) + Math.random().toString(36).slice(2);
 }
 
 document.addEventListener('DOMContentLoaded', ()=> { 
@@ -87,10 +23,15 @@ document.addEventListener('DOMContentLoaded', ()=> {
   const closeModal = document.getElementById('close-modal');
 
   noteForm.addEventListener('submit', function(ev){
-    addNewNote();
+    ev.preventDefault();
+    const titleInput = noteForm.querySelector('#title-input').value;
+    const bodyInput = noteForm.querySelector('#note-body-input').value;
+
+    insertNewNote(titleInput, bodyInput);
+    render();
+
     noteForm.reset();
     modal.classList.remove('show')
-    ev.preventDefault();
   })
 
   // expand menu sidebar
